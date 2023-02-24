25e2df879c35e74a844f5e845bc34ea05ce76441
nestorLicona0896 <nestor_licona@hotmail.com>
2023-02-23 19:53:56 -0600

    change digit field when its value is numeric

--- LOGIN 3.0/JS/code_section.js
+++ LOGIN 3.0/JS/code_section.js
@@ -1,29 +1,34 @@
 /* This function add to every code field on form an oninput event and a event listener when the user requires to 
-delete or erase a digit from a field*/ 
+delete or erase a digit from a field */ 
 function digitFieldFocus() {
     var header = document.getElementById("code-container");
     var fields = header.getElementsByClassName("digit");
-
     for (var i = 0; i < fields.length; i++) {
         //fields[i].oninput =  function(){ changeFieldFocus(this) };
-        fields[i].addEventListener('keyup',  function(event) { 
+        fields[i].addEventListener('keyup', function (event) {
             const key = event.key; // const {key} = event; ES6+
-            if (key === "Delete" || key === "Backspace" ) {
-                this.previousElementSibling.focus(); 
+            if (key === "Delete" || key === "Backspace") {
+                this.previousElementSibling.focus();
             } else {
-                changeFieldFocus(this);
+                changeFieldFocus(this, key);
             }
         });
     }
 }
 
-/* This function add to every code field that is next to the current field a focus,
-but before that, it checks that the value also is numeric */
-function changeFieldFocus(field) {
+/* This function add to every code field that is next to the current field a focus */
+function changeFieldFocus(field, key) {
     var id = field.getAttribute('id');
-    if (document.getElementById(id).value == 0  || document.getElementById(id).value == 1 || document.getElementById(id).value == 2 || document.getElementById(id).value == 3 || document.getElementById(id).value == 4 || document.getElementById(id).value== 5 || document.getElementById(id).value == 6 || document.getElementById(id).value == 7 || document.getElementById(id).value == 8 || document.getElementById(id).value == 9) {
-        if(document.getElementById(id).value != "" && document.getElementById(id).value != " "){
-            document.getElementById(id).nextElementSibling.focus();
+    //if (document.getElementById(id).value == 0 || document.getElementById(id).value == 1 || document.getElementById(id).value == 2 || document.getElementById(id).value == 3 || document.getElementById(id).value == 4 || document.getElementById(id).value == 5 || document.getElementById(id).value == 6 || document.getElementById(id).value == 7 || document.getElementById(id).value == 8 || document.getElementById(id).value == 9) {
+    if (key == 0 || key == 1 || key == 2 || key == 3 || key == 4 || key == 5 || key == 6 || key == 7 || key == 8 || key == 9) {
+
+        if (document.getElementById(id).value != " " && key != " ") {
+            document.getElementById(id).value = key;
+            if (document.getElementById(id).nextElementSibling != null) {
+                document.getElementById(id).nextElementSibling.focus();
+            } else {
+                document.getElementById("verify-button").focus();
+            }
         } else {
             document.getElementById(id).value = "";
         }
@@ -33,29 +38,31 @@ function changeFieldFocus(field) {
 }
 
 // verifies the validation of the code sended to the registered email:
-function verifyCode(){
+function verifyCode() {
     //validations....if(verify() == true) {.......}
-
-    displayResultSection();
+    document.getElementById("verify-button").onclick = function () {
+        displayResultSection();
+    };
 }
 
 // make the result section visible with the propper message according to the verification status:
 function displayResultSection() {
-    document.getElementById("verify-button").onclick = function () {
-        var form = document.getElementById("portal");
-        var xhtml = new XMLHttpRequest();
+   
+    var form = document.getElementById("portal");
+    var xhtml = new XMLHttpRequest();
 
-        xhtml.open('GET', 'result_section.html', true);
-        xhtml.onreadystatechange = function () {
-            form.innerHTML = this.responseText;
-            var script = document.createElement("script");
-            script.innerHTML = "displayPortal();";
-            form.appendChild(script);
-        };
-        xhtml.send();
+    xhtml.open('GET', 'result_section.html', true);
+    xhtml.onreadystatechange = function () {
+        form.innerHTML = this.responseText;
+        var script = document.createElement("script");
+        script.innerHTML = "displayHomePortal();";
+        form.appendChild(script);
     };
+    xhtml.send();
+
 }
 
-/* 
-detects if the backspace or delete button is pressed on the document.getElementById(id)board when a digit-field is focus so 
-its erase the value, but also turn the focus onto the previus digit field*/
+
+
+
+
--- LOGIN 3.0/JS/result_section.js
+++ LOGIN 3.0/JS/result_section.js
@@ -1,27 +1,13 @@
 // makes that the login-section of the form gets displayed but also loads all the functions of the new section:
-function displayPortal() {
+function displayHomePortal() {
     /*fetch('login.html')
     .then(response => response.text())
     .then(text => document.getElementById('portal').innerHTML = text);
     */
     var continueButton = document.getElementById("continue-button");
     continueButton.onclick = function () {
-        displayHomeSection();
+        displayHomeSection(); // makes a call in the portal_form.js file to the displayHomeSection() function
     };
-    
 }
 
-/* function displayHomeSection() {
-    var form = document.getElementById("portal");
-    var xhtml = new XMLHttpRequest();
-
-    xhtml.open('GET', 'home_section.html', true);
-    xhtml.onreadystatechange = function () {
-        form.innerHTML = this.responseText;
-        var script = document.createElement("script");
-        script.innerHTML = "displaySection();";
-        form.appendChild(script);
-    };
-    xhtml.send();
-} */
 
